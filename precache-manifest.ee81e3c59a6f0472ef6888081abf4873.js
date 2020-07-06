93c34cf7ef63ae16a6148d0ceb0add2ba1569b03
Raghu412 <matrixbender.412@gmail.com>
2020-07-06 12:45:54 +0530

    Updates

---
+++ precache-manifest.ee81e3c59a6f0472ef6888081abf4873.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c985ac2961c788abef8f4445a3d17e8",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "9b7d72803131681cc4ab",
+    "url": "/resume/static/css/main.5619e8a0.chunk.css"
+  },
+  {
+    "revision": "31ed4fada16086d09cc8",
+    "url": "/resume/static/js/2.cf6cdb38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume/static/js/2.cf6cdb38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b7d72803131681cc4ab",
+    "url": "/resume/static/js/main.4f027f97.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  },
+  {
+    "revision": "a37889f457f148e6fd03ea37a097dea4",
+    "url": "/resume/static/media/AutoAE.a37889f4.png"
+  },
+  {
+    "revision": "f0e619fd36a6406c4ddcdaed5fdf9bd7",
+    "url": "/resume/static/media/CoV-2-Lifecycle.f0e619fd.png"
+  },
+  {
+    "revision": "99a2aed584ca0011475d320c6addc8b5",
+    "url": "/resume/static/media/ManageMyGym.99a2aed5.png"
+  },
+  {
+    "revision": "f3d010bc43d5d642a442cf49ec8944d2",
+    "url": "/resume/static/media/PhoenixMusic.f3d010bc.png"
+  },
+  {
+    "revision": "d54a98252bde55a60999189ef012b0fb",
+    "url": "/resume/static/media/ShineMyCars.d54a9825.png"
+  },
+  {
+    "revision": "507595f222a3b017ffdde2ff23ddeda5",
+    "url": "/resume/static/media/comprehend.507595f2.png"
+  },
+  {
+    "revision": "df16ad2202f21e78bf5a2ea61393bb92",
+    "url": "/resume/static/media/git.df16ad22.png"
+  },
+  {
+    "revision": "cd17ad595d26e7e435145df368cf5b47",
+    "url": "/resume/static/media/goto.cd17ad59.png"
+  },
+  {
+    "revision": "4d9222ef3dc56dca622ce4662c62f6d1",
+    "url": "/resume/static/media/instagram.4d9222ef.png"
+  },
+  {
+    "revision": "e4dd4db88636efab0d4854e13fb4403b",
+    "url": "/resume/static/media/linkedin.e4dd4db8.png"
+  },
+  {
+    "revision": "f4f29e3f2576b48753848e18d99c087e",
+    "url": "/resume/static/media/logo.f4f29e3f.png"
+  },
+  {
+    "revision": "25a07306bfa4e0509126c3d972dd5759",
+    "url": "/resume/static/media/profile.25a07306.png"
+  },
+  {
+    "revision": "92a684808f82ba93603aba4331096697",
+    "url": "/resume/static/media/read.92a68480.png"
+  },
+  {
+    "revision": "b8635f152f99a9248f9126f2299debc1",
+    "url": "/resume/static/media/verified.b8635f15.png"
+  },
+  {
+    "revision": "2ca7de4bb829c2811a2a5aa5dca0487b",
+    "url": "/resume/static/media/write.2ca7de4b.png"
+  }
+]);
